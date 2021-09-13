88dc7dfd91431198e5f0da2e509dbcd775fcfd73
rysiek21 <rysiek21@users.noreply.github.com>
2021-09-13 20:59:38 +0200

    Add potion recipe

--- src/me/rysiek21/RadiationBorder/Main.java
+++ src/me/rysiek21/RadiationBorder/Main.java
@@ -1,8 +1,14 @@
 package me.rysiek21.RadiationBorder;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -20,6 +26,7 @@ public class Main extends JavaPlugin {
 		PluginListeners pluginListeners = new PluginListeners();
 		registerEvents(this, pluginListeners);
 		pluginListeners.DamagePlayer();
+		CreateRecipe();
 	}
 	
 	public static void registerEvents(org.bukkit.plugin.Plugin plugin, Listener... listeners) {
@@ -34,5 +41,28 @@ public class Main extends JavaPlugin {
 	
 	public static FileConfiguration getConfigFile() {
 		return config;
+	}
+	
+	void CreateRecipe() {
+		ItemStack item = new ItemStack(Material.POTION);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(ChatColor.GREEN + "Anti-Radiation potion");
+		item.setItemMeta(meta);
+		
+		NamespacedKey key = new NamespacedKey(this, "radiation_potion");
+		ShapedRecipe recipe = new ShapedRecipe(key, item);
+		recipe.shape("ABC","DEF","GHI");
+		recipe.setIngredient('A', Material.getMaterial(config.getString("recipe.1")));
+		recipe.setIngredient('B', Material.getMaterial(config.getString("recipe.2")));
+		recipe.setIngredient('C', Material.getMaterial(config.getString("recipe.3")));
+		recipe.setIngredient('D', Material.getMaterial(config.getString("recipe.4")));
+		recipe.setIngredient('E', Material.getMaterial(config.getString("recipe.5")));
+		recipe.setIngredient('F', Material.getMaterial(config.getString("recipe.6")));
+		recipe.setIngredient('G', Material.getMaterial(config.getString("recipe.7")));
+		recipe.setIngredient('H', Material.getMaterial(config.getString("recipe.8")));
+		recipe.setIngredient('I', Material.getMaterial(config.getString("recipe.9")));
+		
+		Bukkit.addRecipe(recipe);
+		
 	}
 }
